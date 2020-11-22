58f4011f43b2127cc31f8943779a76cc086fb9c2
mishel254 <sonimichie@gmail.com>
2020-11-23 01:16:21 +0300

    typo

--- clone/views.py
+++ clone/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import User,Post,Bio
 
 # Create your views here.
 def welcome(request):
     return render(request,'index.html')
 
 def  instagram(request):
-    return render (request,'interface.html')
+    username = User.objects.all()
+    context = {'username':username}
+    return render (request,'interface.html',context)
